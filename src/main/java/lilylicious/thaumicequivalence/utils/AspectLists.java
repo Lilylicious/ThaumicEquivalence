603aa2125241bffc6ea8a80acacc1643534914fd
Dwargkiller <peter.hammerskog@gmail.com>
2015-10-14 19:08:17 +0200

    Replacing placeholder recipes

--- src/main/java/lilylicious/thaumicequivalence/utils/AspectLists.java
+++ src/main/java/lilylicious/thaumicequivalence/utils/AspectLists.java
@@ -5,26 +5,43 @@ import thaumcraft.api.aspects.AspectList;
 
 public class AspectLists
 {
-	public static AspectList placeholderAspectList = new AspectList().add(Aspect.FIRE, 1);
+	private static AspectList placeholderAspectList = new AspectList().add(Aspect.FIRE, 1);
 
 	//Research
 	public static AspectList basicEMCTheoryResearch = placeholderAspectList;
 	public static AspectList philoStoneResearch = placeholderAspectList;
-	public static AspectList transTableResearch = placeholderAspectList;
+	public static AspectList transTableResearch =  placeholderAspectList;
 	public static AspectList darkMatterResearch = placeholderAspectList;
-	public static AspectList redMatterResearch = placeholderAspectList;
 	public static AspectList dmAxeResearch = placeholderAspectList;
+	public static AspectList dmHammerResearch = placeholderAspectList;
+	public static AspectList dmHoeResearch = placeholderAspectList;
+	public static AspectList dmShearsResearch = placeholderAspectList;
+	public static AspectList dmShovelResearch = placeholderAspectList;
+	public static AspectList dmSwordResearch = placeholderAspectList;
+	public static AspectList dmPickResearch = placeholderAspectList;
+	public static AspectList dmHelmResearch = placeholderAspectList;
+	public static AspectList dmChestResearch = placeholderAspectList;
+	public static AspectList dmLegsResearch = placeholderAspectList;
+	public static AspectList dmBootsResearch = placeholderAspectList;
+	public static AspectList covalenceResearch = placeholderAspectList;
+	public static AspectList repairResearch = placeholderAspectList;
 	
 	//Crafting
-	public static AspectList philoStoneCrafting = placeholderAspectList;
-	public static AspectList transTableCrafting = placeholderAspectList;
-	public static AspectList darkMatterCrafting = placeholderAspectList;
-	public static AspectList redMatterBlockCrafting = placeholderAspectList;
-	public static AspectList dmAxeCrafting = placeholderAspectList;
-	public static AspectList dmHammerCrafting = placeholderAspectList;
-	public static AspectList dmHoeCrafting = placeholderAspectList;
-	public static AspectList dmShearsCrafting = placeholderAspectList;
-	public static AspectList dmShovelCrafting = placeholderAspectList;
-	public static AspectList dmSwordCrafting = placeholderAspectList;
-	public static AspectList dmPickCrafting = placeholderAspectList;
+	public static AspectList philoStoneCrafting = new AspectList().add(Aspect.EXCHANGE, 16).add(Aspect.ENERGY, 16);
+	public static AspectList transTableCrafting = new AspectList().add(Aspect.ORDER, 15).add(Aspect.ENTROPY, 15);
+	public static AspectList darkMatterCrafting = new AspectList().add(Aspect.EXCHANGE, 20).add(Aspect.ENERGY, 10).add(Aspect.DARKNESS, 10);
+	public static AspectList dmAxeCrafting = new AspectList().add(Aspect.EXCHANGE, 16).add(Aspect.TREE, 8);
+	public static AspectList dmHammerCrafting = new AspectList().add(Aspect.EXCHANGE, 16).add(Aspect.MINE, 8);
+	public static AspectList dmHoeCrafting = new AspectList().add(Aspect.EXCHANGE, 16).add(Aspect.HARVEST, 8);
+	public static AspectList dmShearsCrafting = new AspectList().add(Aspect.EXCHANGE, 16).add(Aspect.TOOL, 4);
+	public static AspectList dmShovelCrafting = new AspectList().add(Aspect.EXCHANGE, 16).add(Aspect.EARTH, 8);
+	public static AspectList dmSwordCrafting = new AspectList().add(Aspect.EXCHANGE, 16).add(Aspect.TOOL, 4).add(Aspect.ENERGY, 4);
+	public static AspectList dmPickCrafting = new AspectList().add(Aspect.EXCHANGE, 16).add(Aspect.TOOL, 8);
+	public static AspectList dmFeetCrafting = new AspectList().add(Aspect.EXCHANGE, 16).add(Aspect.ARMOR, 16).add(Aspect.METAL, 16);
+	public static AspectList dmLegsCrafting = new AspectList().add(Aspect.EXCHANGE, 16).add(Aspect.ARMOR, 24).add(Aspect.METAL, 16);
+	public static AspectList dmChestCrafting = new AspectList().add(Aspect.EXCHANGE, 16).add(Aspect.ARMOR, 32).add(Aspect.METAL, 16);
+	public static AspectList dmHelmCrafting = new AspectList().add(Aspect.EXCHANGE, 16).add(Aspect.ARMOR, 16).add(Aspect.METAL, 16);
+	public static AspectList covalenceLowCrafting =  new AspectList().add(Aspect.ORDER, 4).add(Aspect.ENTROPY, 4);
+	public static AspectList covalenceMedCrafting = new AspectList().add(Aspect.EXCHANGE, 3);
+	public static AspectList covalenceHighCrafting = new AspectList().add(Aspect.EXCHANGE, 5);
 }
--- src/main/java/lilylicious/thaumicequivalence/utils/RecipeLists.java
+++ src/main/java/lilylicious/thaumicequivalence/utils/RecipeLists.java
@@ -1,20 +1,27 @@
 package lilylicious.thaumicequivalence.utils;
 
+import moze_intel.projecte.gameObjs.ObjHandler;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import thaumcraft.api.ItemApi;
 
 public class RecipeLists
 {
-	public static ItemStack[] placeholder = new ItemStack[]{new ItemStack(Blocks.stone), new ItemStack(Blocks.stone), new ItemStack(Blocks.stone), new ItemStack(Blocks.stone)};
-	public static ItemStack[] philoStone = placeholder;
+	private static ItemStack[] placeholder = new ItemStack[]{new ItemStack(Blocks.stone), new ItemStack(Blocks.stone), new ItemStack(Blocks.stone), new ItemStack(Blocks.stone)};
+	public static ItemStack[] philoStone = new ItemStack[]{new ItemStack(Items.redstone), new ItemStack(Items.glowstone_dust), new ItemStack(Items.redstone), new ItemStack(Items.glowstone_dust), new ItemStack(Items.redstone), new ItemStack(Items.glowstone_dust), new ItemStack(Items.redstone), new ItemStack(Items.glowstone_dust)};
 	public static ItemStack[] darkMatter = new ItemStack[]{ItemApi.getItem("itemResource", 16),ItemApi.getItem("itemShard", 6), ItemApi.getItem("itemResource", 16),ItemApi.getItem("itemShard", 6), ItemApi.getItem("itemResource", 16),ItemApi.getItem("itemShard", 6), ItemApi.getItem("itemResource", 16),ItemApi.getItem("itemShard", 6)};
-	public static ItemStack[] redMatter = placeholder;
-	public static ItemStack[] dmAxe = placeholder;
-	public static ItemStack[] dmHammer  = placeholder;
-	public static ItemStack[] dmHoe  = placeholder;
-	public static ItemStack[] dmShears  = placeholder;
-	public static ItemStack[] dmShovel  = placeholder;
-	public static ItemStack[] dmSword  = placeholder;
-	public static ItemStack[] dmPick  = placeholder;
+	public static ItemStack[] dmAxe = new ItemStack[]{new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0)};
+	public static ItemStack[] dmHammer  = new ItemStack[]{new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0)};
+	public static ItemStack[] dmHoe  = new ItemStack[]{new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0)};
+	public static ItemStack[] dmShears  = new ItemStack[]{new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0)};
+	public static ItemStack[] dmShovel  = new ItemStack[]{new ItemStack(ObjHandler.matter, 1, 0)};
+	public static ItemStack[] dmSword  = new ItemStack[]{new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0)};
+	public static ItemStack[] dmPick  = new ItemStack[]{new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0)};
+	public static ItemStack[] dmHelm = new ItemStack[]{new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0)};
+	public static ItemStack[] dmChest = new ItemStack[]{new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0)};
+	public static ItemStack[] dmLegs = new ItemStack[]{new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0)};
+	public static ItemStack[] dmFeet = new ItemStack[]{new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0), new ItemStack(ObjHandler.matter, 1, 0)};
+	public static ItemStack[] covalenceMed =  new ItemStack[]{new ItemStack(Items.iron_ingot), new ItemStack(Items.coal), new ItemStack(Items.redstone), new ItemStack(Items.iron_ingot), new ItemStack(Items.coal), new ItemStack(Items.redstone)};
+	public static ItemStack[] covalenceHigh = new ItemStack[]{new ItemStack(Items.gold_ingot), new ItemStack(Items.coal), new ItemStack(Items.redstone), new ItemStack(Items.gold_ingot), new ItemStack(Items.coal), new ItemStack(Items.redstone)};
 }
